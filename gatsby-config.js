8546eb88f675b6f48658c219f60ef9b06440f7fc
Lindsay Wardell <lindsay.wardell@daimler.com>
2019-06-06 16:01:40 -0700

    Tweaks to settings

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Lindsay Wardell`,
+    description: `Blog and Portfolio`,
     author: `@gatsbyjs`,
   },
   plugins: [
@@ -19,8 +19,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Lindsay Wardell`,
+        short_name: `lindsaykwardell`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
