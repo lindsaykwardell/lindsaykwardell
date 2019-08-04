14f2ee70970c76fbfa8c883394ec8b6b0daea45a
Lindsay Wardell <three060@gmail.com>
2019-08-03 23:15:22 -0700

    Tweak to subscription

--- src/components/Mailchimp/Mailchimp.tsx
+++ src/components/Mailchimp/Mailchimp.tsx
@@ -1,23 +1,40 @@
 import MailchimpSubscribe from "react-mailchimp-subscribe"
 import React, { useState, ChangeEvent } from "react"
-import Button from "../Button/Button";
+import Button from "../Button/Button"
 import Input from "../Input/Input"
 
 const Mailchimp = () => {
-  const [email, setEmail] = useState<string>();
+  const [email, setEmail] = useState<string>()
 
   return (
     <MailchimpSubscribe
-      url={"https://gmail.us3.list-manage.com/subscribe/post?u=0a6b50e4167f49fa4b57a452a&amp;id=e20e17d6bc"}
+      url={
+        "https://gmail.us3.list-manage.com/subscribe/post?u=0a6b50e4167f49fa4b57a452a&amp;id=e20e17d6bc"
+      }
       render={({ subscribe, status, message }) => (
         <div className="text-left">
           <span className="text-white px-4 py-2">Subscribe via Email</span>
           <div className="ml-5">
-            <Input type="email" className="my-3 text-black" style={{width: "80%"}} onChange={setEmail} />
-            <Button onClick={() => subscribe({EMAIL: email})} size="sm">Subscribe</Button>
-            {status === "sending" && <div>sending...</div>}
-            {status === "error" && <div style={{ color: "red" }} dangerouslySetInnerHTML={{ __html: message }} />}
-            {status === "success" && <div style={{ color: "green" }}>Subscribed !</div>}
+            <div>
+              <Input
+                type="email"
+                className="text-black"
+                style={{ width: "80%", margin: "10px auto" }}
+                onChange={setEmail}
+              />
+            </div>
+            <Button onClick={() => subscribe({ EMAIL: email })} size="sm">
+              Subscribe
+            </Button>
+            <div className="mt-4 text-sm">
+              {status === "sending" && <div>Sending...</div>}
+              {status === "error" && (
+                <div dangerouslySetInnerHTML={{ __html: message }} />
+              )}
+              {status === "success" && (
+                <div>All done! Watch your email for the next post.</div>
+              )}
+            </div>
           </div>
         </div>
       )}
@@ -25,4 +42,4 @@ const Mailchimp = () => {
   )
 }
 
-export default Mailchimp;
+export default Mailchimp
