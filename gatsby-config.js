cdce3cb8141c9b06e1244dfc742c08c29575d453
Lindsay Wardell <three060@gmail.com>
2019-08-04 19:26:53 -0700

    Added google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -161,6 +161,12 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: process.env.GATSBY_GOOGLE_ANALYTICS,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
