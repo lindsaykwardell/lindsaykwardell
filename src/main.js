64f0bb1680ddecc1972541cf5e1437cdad89a316
Lindsay Wardell <three060@gmail.com>
2020-07-23 07:17:47 -0700

    Updated font

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
-import "~/assets/main.css";
+import "~/assets/main.postcss";
 import 'prismjs/themes/prism.css'
 
 import Icon from 'vue-awesome/components/Icon'
@@ -9,7 +9,7 @@ import DefaultLayout from "~/layouts/Default.vue";
 export default function(Vue, { router, head, isClient }) {
   head.link.push({
     rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css?family=Lato|Rokkitt&display=swap"
+    href: "https://fonts.googleapis.com/css?family=Lato|Rokkitt|Fira+Sans&display=swap"
   });
   head.script.push({
     src: "https://f.convertkit.com/ckjs/ck.5.js"
