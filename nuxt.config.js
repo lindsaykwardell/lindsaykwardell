5d203139658374e6b23ab4ac446e9a33472becd7
Lindsay Wardell <three060@gmail.com>
2020-11-14 10:06:36 -0800

    Updated favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,7 @@ export default {
       { hid: 'description', name: 'description', content: '' },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/png', href: '/favicon.png' },
       {
         rel: 'stylesheet',
         href:
