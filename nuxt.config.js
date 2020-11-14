8050b3f128e46d944a93af6412e90a09262809a1
Lindsay Wardell <three060@gmail.com>
2020-11-14 09:55:57 -0800

    Fixed nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,8 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'Lindsay Wardell'
+    title: 'Lindsay Wardell',
+    meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
