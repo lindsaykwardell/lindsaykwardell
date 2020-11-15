5455091e3c2d7763aa77056903ae24351730200a
Lindsay Wardell <three060@gmail.com>
2020-11-15 08:11:05 -0800

    Improved index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,68 +1,74 @@
 <template>
-  <div class="container">
-    <div class="flex flex-col items-center">
-      <img
-        :src="$github.user.avatarUrl"
-        :alt="$github.user.name"
-        class="rounded-full shadow-xl w-64 lg:w-auto my-4"
-      />
-      <h1 class="title text-6xl py-3 font-medium">
-        {{ $github.user.name }}
-      </h1>
-      <h2 class="text-3xl lg:text-4xl leading-9 font-medium py-4">
-        {{ $github.user.bio }}
-      </h2>
-      <div class="flex w-full pt-12">
-        <div
-          class="flex-1 flex justify-center items-center p-2 tech-logo-wrapper"
-        >
-          <img
-            class="tech-logo"
-            src="~/assets/images/vue.png"
-            width="100"
-            alt="Vue"
-          />
+  <div class="index-wrapper">
+    <div class="flex flex-wrap justify-between w-full mb-24">
+      <div class="w-full xl:w-1/2 flex flex-col items-center">
+        <img
+          :src="$github.user.avatarUrl"
+          :alt="$github.user.name"
+          class="rounded-full shadow-xl w-64 lg:w-auto my-4"
+        />
+        <h1 class="title text-6xl py-3 font-medium">
+          {{ $github.user.name }}
+        </h1>
+        <h2 class="text-3xl lg:text-4xl leading-9 font-medium py-4">
+          {{ $github.user.bio }}
+        </h2>
+        <div class="flex w-full pt-4">
+          <div
+            class="flex-1 flex justify-center items-center p-2 tech-logo-wrapper"
+          >
+            <img
+              class="tech-logo"
+              src="~/assets/images/vue.png"
+              width="100"
+              alt="Vue"
+            />
+          </div>
+          <div
+            class="flex-1 flex justify-center items-center p-2 tech-logo-wrapper"
+          >
+            <img
+              class="tech-logo"
+              src="~/assets/images/tailwind.png"
+              width="100"
+              alt="Tailwind CSS"
+            />
+          </div>
+          <div
+            class="flex-1 flex justify-center items-center p-2 tech-logo-wrapper"
+          >
+            <img
+              class="tech-logo"
+              src="~/assets/images/typescript.jpg"
+              width="100"
+              alt="Typescript"
+            />
+          </div>
+          <div
+            class="flex-1 flex justify-center items-center p-2 tech-logo-wrapper"
+          >
+            <img
+              class="tech-logo"
+              src="~/assets/images/nodejs.png"
+              width="100"
+              alt="NodeJS"
+            />
+          </div>
+          <div
+            class="flex-1 flex justify-center items-center p-2 tech-logo-wrapper"
+          >
+            <img
+              class="tech-logo"
+              src="~/assets/images/aplus.png"
+              width="100"
+              alt="CompTIA A+ Certified"
+            />
+          </div>
         </div>
-        <div
-          class="flex-1 flex justify-center items-center p-2 tech-logo-wrapper"
-        >
-          <img
-            class="tech-logo"
-            src="~/assets/images/tailwind.png"
-            width="100"
-            alt="Tailwind CSS"
-          />
-        </div>
-        <div
-          class="flex-1 flex justify-center items-center p-2 tech-logo-wrapper"
-        >
-          <img
-            class="tech-logo"
-            src="~/assets/images/typescript.jpg"
-            width="100"
-            alt="Typescript"
-          />
-        </div>
-        <div
-          class="flex-1 flex justify-center items-center p-2 tech-logo-wrapper"
-        >
-          <img
-            class="tech-logo"
-            src="~/assets/images/nodejs.png"
-            width="100"
-            alt="NodeJS"
-          />
-        </div>
-        <div
-          class="flex-1 flex justify-center items-center p-2 tech-logo-wrapper"
-        >
-          <img
-            class="tech-logo"
-            src="~/assets/images/aplus.png"
-            width="100"
-            alt="CompTIA A+ Certified"
-          />
-        </div>
+      </div>
+      <div class="w-full xl:w-1/3 pt-6 xl:pt-0 flex flex-col items-center justify-end">
+        <h2>Latest Post</h2>
+        <ContentItem v-for="item in formattedPosts" :key="item.slug" :item="item" />
       </div>
     </div>
     <div
@@ -75,28 +81,40 @@
 
 <script>
 import SocialLinks from '@/components/SocialLinks'
+import ContentItem from '@/components/ContentItem'
 
 export default {
+  async asyncData({ $content }) {
+    const posts = await $content(`posts`)
+      .sortBy('date', 'desc')
+      .limit(1)
+      .fetch()
+
+    return {
+      posts,
+    }
+  },
+  computed: {
+    formattedPosts() {
+      return this.posts.map((post) => ({
+        ...post,
+        link: `/blog${post.slug}`,
+        image: post.image || this.$github.user.avatarUrl,
+      }))
+    },
+  },
   components: {
     SocialLinks,
+    ContentItem,
   },
 }
 </script>
 
 <style lang="postcss" scoped>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-@apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
-.container {
-  position: relative;
-  margin: 0 auto;
+.index-wrapper {
+  @apply relative w-full flex justify-center items-center text-center px-8;
+  /* margin: 0 auto; */
   min-height: calc(100vh - 110px);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
 }
 
 .title {
