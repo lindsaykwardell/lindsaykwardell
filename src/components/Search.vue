027e6ba6cf39473e3bc216f83534c62e406181e5
Lindsay Wardell <three060@gmail.com>
2022-06-16 09:59:32 -0700

    Move Content to Search.vue

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,7 +1,79 @@
 <script setup lang="ts">
+import Fuse from 'fuse.js'
 import { ref, computed } from 'vue'
-import { Content } from '../api/Content.mjs'
 import BlogPostPreview from './BlogPostPreview.vue'
+
+class Content {
+  #media
+
+  constructor(media) {
+    this.#media = media
+  }
+
+  get toList() {
+    return this.#media
+  }
+
+  get length() {
+    return this.#media.length
+  }
+
+  get locations() {
+    return this.#media
+      .filter(
+        (post, index) =>
+          post.name &&
+          this.#media.findIndex((e) => e.name === post.name) === index
+      )
+      .map((post) => post.name)
+  }
+
+  get byLocation() {
+    return this.#media.reduce((shows, show) => {
+      if (show.name) {
+        if (!(show.name in shows)) {
+          shows[show.name] = []
+        }
+
+        shows[show.name].push(show)
+      }
+
+      return shows
+    }, {})
+  }
+
+  get podcasts() {
+    return new Content(this.#media.filter((media) => media.type === 'podcast'))
+  }
+
+  get videos() {
+    return new Content(this.#media.filter((media) => media.type === 'video'))
+  }
+
+  get meetups() {
+    return new Content(this.#media.filter((media) => media.type === 'meetup'))
+  }
+
+  get blogs() {
+    return new Content(this.#media.filter((media) => media.type === 'blog'))
+  }
+
+  get hosted() {
+    return new Content(this.#media.filter((media) => media.host))
+  }
+
+  get guest() {
+    return new Content(this.#media.filter((media) => !media.host))
+  }
+
+  search(query) {
+    const results = new Fuse(this.#media, {
+      keys: ['title', 'name', 'snippet', 'pubDate'],
+    }).search(query)
+
+    return new Content(results.map((results) => results.item))
+  }
+}
 
 const props = defineProps({
   content: {
