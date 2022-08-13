a8a53b611de98a6ff311824031e946e026b51382
Lindsay Wardell <three060@gmail.com>
2022-08-13 08:38:00 -0700

    cache podcast details (no need to keep fetching them any more)

--- functions/podcasts.js
+++ functions/podcasts.js
@@ -4,69 +4,73 @@ import naturalOrder from 'natural-order'
 const parser = new Parser()
 
 export async function handler(event, context) {
-  try {
-    const [vov, modernWeb, sit] = await Promise.all([
-      parser
-        .parseURL('https://feeds.feedwrench.com/views-on-vue.rss')
-        .catch(() => ({
-          items: [],
-        })),
-      parser
-        .parseURL('https://feed.podbean.com/modernweb/feed.xml')
-        .catch(() => ({
-          items: [],
-        })),
-      parser
-        .parseURL('https://feeds.feedwrench.com/shesintech.rss')
-        .catch(() => ({
-          items: [],
-        })),
-    ])
-
-    return {
-      statusCode: 200,
-      body: JSON.stringify(
-        naturalOrder([
-          ...vov.items
-            .filter((episode) => episode.contentSnippet.includes('Lindsay'))
-            .map((episode) => ({
-              ...episode,
-              image: vov.itunes.image,
-              snippet: episode.contentSnippet.split('\n')[0],
-              type: 'podcast',
-              host: true,
-              name: 'Views on Vue'
-            })),
-          ...modernWeb.items
-            .filter((episode) => episode.contentSnippet.includes('Lindsay'))
-            .map((episode) => ({
-              ...episode,
-              image: modernWeb.image?.url,
-              snippet: episode.contentSnippet.split('\n')[0],
-              type: 'podcast',
-              host: true,
-              name: 'Modern Web'
-            })),
-          ...sit.items
-            .filter((episode) => episode.contentSnippet.includes('Lindsay'))
-            .map((episode) => ({
-              ...episode,
-              image: sit.itunes.image,
-              snippet: episode.contentSnippet.split('\n')[0],
-              type: 'podcast',
-              host: true,
-              name: 'She\'s In Tech'
-            })),
-        ])
-          .orderBy('desc')
-          .sort(['pubDate'])
-      ),
-    }
-  } catch (err) {
-    console.log(err) // output to netlify function log
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ msg: err.message }), // Could be a custom message or object i.e. JSON.stringify(err)
-    }
+  return {
+    statusCode: 204,
+    body: [],
   }
+  // try {
+  //   const [vov, modernWeb, sit] = await Promise.all([
+  //     parser
+  //       .parseURL('https://feeds.feedwrench.com/views-on-vue.rss')
+  //       .catch(() => ({
+  //         items: [],
+  //       })),
+  //     parser
+  //       .parseURL('https://feed.podbean.com/modernweb/feed.xml')
+  //       .catch(() => ({
+  //         items: [],
+  //       })),
+  //     parser
+  //       .parseURL('https://feeds.feedwrench.com/shesintech.rss')
+  //       .catch(() => ({
+  //         items: [],
+  //       })),
+  //   ])
+
+  //   return {
+  //     statusCode: 200,
+  //     body: JSON.stringify(
+  //       naturalOrder([
+  //         ...vov.items
+  //           .filter((episode) => episode.contentSnippet.includes('Lindsay'))
+  //           .map((episode) => ({
+  //             ...episode,
+  //             image: vov.itunes.image,
+  //             snippet: episode.contentSnippet.split('\n')[0],
+  //             type: 'podcast',
+  //             host: true,
+  //             name: 'Views on Vue'
+  //           })),
+  //         ...modernWeb.items
+  //           .filter((episode) => episode.contentSnippet.includes('Lindsay'))
+  //           .map((episode) => ({
+  //             ...episode,
+  //             image: modernWeb.image?.url,
+  //             snippet: episode.contentSnippet.split('\n')[0],
+  //             type: 'podcast',
+  //             host: true,
+  //             name: 'Modern Web'
+  //           })),
+  //         ...sit.items
+  //           .filter((episode) => episode.contentSnippet.includes('Lindsay'))
+  //           .map((episode) => ({
+  //             ...episode,
+  //             image: sit.itunes.image,
+  //             snippet: episode.contentSnippet.split('\n')[0],
+  //             type: 'podcast',
+  //             host: true,
+  //             name: 'She\'s In Tech'
+  //           })),
+  //       ])
+  //         .orderBy('desc')
+  //         .sort(['pubDate'])
+  //     ),
+  //   }
+  // } catch (err) {
+  //   console.log(err) // output to netlify function log
+  //   return {
+  //     statusCode: 500,
+  //     body: JSON.stringify({ msg: err.message }), // Could be a custom message or object i.e. JSON.stringify(err)
+  //   }
+  // }
 }
