0eb2ca78aad4b64d65e6255b0baca5b35ab52f7f
Lindsay Wardell <three060@gmail.com>
2022-11-17 10:39:21 -0800

    Switch to Netlify adapter for deploys, minor style adajustments

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,15 @@ import vue from '@astrojs/vue';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
+import netlify from "@astrojs/netlify/functions";
+
+// https://astro.build/config
 export default defineConfig({
   integrations: [vue(), tailwind({
     config: {
       path: 'tailwind.config.cjs'
     }
-  })]
+  })],
+  output: "server",
+  adapter: netlify()
 });
