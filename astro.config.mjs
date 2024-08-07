065dc1e0f2cc796abbbb9fe134773907f765defd
Lindsay Wardell <three060@gmail.com>
2024-08-07 22:59:24 +0000

    Starting v5

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,8 @@
 import { defineConfig } from 'astro/config';
-import vue from '@astrojs/vue';
+
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-import netlify from "@astrojs/netlify/functions";
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [vue(), tailwind({
-    config: {
-      path: 'tailwind.config.cjs'
-    }
-  })],
-  output: "server",
-  adapter: netlify()
+  integrations: [tailwind()]
 });
--- src/api/details.mjs
+++
@@ -1,6 +0,0 @@
-export default {
-    title: 'Lindsay Wardell',
-    description: 'Programmer, writer, mother. She/Her. Interested in Vue, Typescript, Node.js, and Elm.',
-    image: 'https://lindsaykwardell.com/lindsay.jpg',
-    permalink: 'https://lindsaykwardell.com'
-}
