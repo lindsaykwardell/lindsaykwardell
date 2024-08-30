77542beda083a2a8fb0fc86d253954bf89aea51e
Lindsay Wardell <three060@gmail.com>
2024-08-30 18:51:43 +0000

    Fixes to transitions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
-import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import netlify from '@astrojs/netlify';
 
-import netlify from '@astrojs/netlify'
+import simpleStackQuery from "simple-stack-query";
 
 // https://astro.build/config
 export default defineConfig({
   experimental: {
     contentLayer: true,
-    contentIntellisense: true,
+    contentIntellisense: true
   },
-  integrations: [tailwind()],
+  integrations: [tailwind(), simpleStackQuery()],
   output: 'server',
-  adapter: netlify(),
-})
+  adapter: netlify()
+});
